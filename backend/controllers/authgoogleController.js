13c8c3f6e86657c6cbe6ac4a5683cb6a3e397038
Sarah Poulin <threehappypenguins@gmail.com>
2024-06-22 22:51:30 -0300

    Protected routes for Google Auth.

--- backend/controllers/authgoogleController.js
+++ backend/controllers/authgoogleController.js
@@ -1,72 +1,85 @@
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const GoogleUser = require("../models/usergoogleModel");
+const { protect } = require("../controllers/authController")
 
 // Controller function to initiate Google OAuth authentication
-exports.googleAuth = passport.authenticate("google", {});
+exports.googleAuth = [
+  protect,
+  passport.authenticate("google", {}),
+];
 
 // Callback function for Google OAuth authentication
-exports.googleAuthCallback = (req, res, next) => {
-  passport.authenticate("google", async (err, user, info) => {
-    if (err) {
-      console.error("Error during authentication:", err);
-      return next(err);
-    }
-    if (!user) {
-      return res.redirect("http://localhost:3000/connect?error=Login failed");
-    }
-    req.login(user, async (loginErr) => {
-      if (loginErr) {
-        console.error("Error during login:", loginErr);
-        return next(loginErr);
+exports.googleAuthCallback = [
+  protect,
+  (req, res, next) => {
+    passport.authenticate("google", async (err, user, info) => {
+      if (err) {
+        console.error("Error during authentication:", err);
+        return next(err);
       }
+      if (!user) {
+        return res.redirect("http://localhost:3000/connect?error=Login failed");
+      }
+      req.login(user, async (loginErr) => {
+        if (loginErr) {
+          console.error("Error during login:", loginErr);
+          return next(loginErr);
+        }
 
-      // Generate JWT
-      const accessToken = jwt.sign({ id: user._id }, process.env.JWT_GOOGLE_SECRET, {
-        expiresIn: "1h",
+        // Generate JWT
+        const accessToken = jwt.sign({ id: user._id }, process.env.JWT_GOOGLE_SECRET, {
+          expiresIn: "1h",
+        });
+        const refreshToken = jwt.sign(
+          { id: user._id },
+          process.env.JWT_GOOGLE_REFRESH_SECRET,
+          { expiresIn: "30d" }
+        );
+
+        // Store refresh token in the database
+        user.refreshToken = refreshToken;
+        await user.save();
+
+        // Redirect with tokens as query parameters
+        res.redirect(
+          `http://localhost:3000/connect?accessToken=${accessToken}&refreshToken=${refreshToken}`
+        );
       });
-      const refreshToken = jwt.sign(
-        { id: user._id },
-        process.env.JWT_GOOGLE_REFRESH_SECRET,
-        { expiresIn: "30d" }
-      );
-
-      // Store refresh token in the database
-      user.refreshToken = refreshToken;
-      await user.save();
-
-      // Redirect with tokens as query parameters
-      res.redirect(
-        `http://localhost:3000/connect?accessToken=${accessToken}&refreshToken=${refreshToken}`
-      );
-    });
-  })(req, res, next);
-};
+    })(req, res, next);
+  }
+];
 
 // Controller function for user logout
-exports.logout = (req, res) => {
-  req.logout(() => {
-    //    res.redirect("/"); // Redirect to the home page after logout
-    res.send("Logout successful!");
-  });
-};
+exports.logout = [
+  protect,
+  (req, res) => {
+    req.logout(() => {
+      //    res.redirect("/"); // Redirect to the home page after logout
+      res.send("Logout successful!");
+    });
+  }
+];
 
 // Controller for checking connection status
-exports.status = async (req, res) => {
-  try {
-    if (req.isAuthenticated()) {
-      // Use the GoogleUser model to find the user by _id
-      const user = await GoogleUser.findById(req.user._id);
+exports.status = [
+  protect,
+  async (req, res) => {
+    try {
+      if (req.isAuthenticated()) {
+        // Use the GoogleUser model to find the user by _id
+        const user = await GoogleUser.findById(req.user._id);
 
-      if (!user || !user.accessToken) {
-        return res.json({ connected: false });
+        if (!user || !user.accessToken) {
+          return res.json({ connected: false });
+        }
+        res.json({ connected: true });
+      } else {
+        res.json({ connected: false });
       }
-      res.json({ connected: true });
-    } else {
-      res.json({ connected: false });
+    } catch (err) {
+      console.error("Error checking status:", err);
+      res.status(500).json({ error: "Internal server error" });
     }
-  } catch (err) {
-    console.error("Error checking status:", err);
-    res.status(500).json({ error: "Internal server error" });
   }
-};
+];
--- backend/routes/authgoogle.js
+++ backend/routes/authgoogle.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const authgoogleController = require("../controllers/authgoogleController");
+const { protect } = require("../controllers/authController");
 
 // Route for initiating Google OAuth authentication
-router.get("/", authgoogleController.googleAuth);
+router.get("/", protect, authgoogleController.googleAuth);
 
 // Route for handling the OAuth callback
-router.get("/callback", authgoogleController.googleAuthCallback);
+router.get("/callback", protect, authgoogleController.googleAuthCallback);
 
 // Route for user logout
-router.get("/logout", authgoogleController.logout);
+router.get("/logout", protect, authgoogleController.logout);
 
 // Route for checking connection status
-router.get("/status", authgoogleController.status);
+router.get("/status", protect, authgoogleController.status);
 
 // Route for refreshing the token
 //router.post("/refresh", authgoogleController.refreshToken);
