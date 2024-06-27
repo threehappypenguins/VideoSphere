e28a4beed6d50b78e64ea9209ff38d4da0a799c9
Sarah Poulin <threehappypenguins@gmail.com>
2024-06-26 23:20:07 -0300

    Added userId from userModel.

--- backend/models/usergoogleModel.js
+++ backend/models/usergoogleModel.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
 const usergoogleSchema = new mongoose.Schema({
   googleId: String,
   accessToken: String,
   refreshToken: String,
+  userId: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
 module.exports = mongoose.model("GoogleUser", usergoogleSchema);
